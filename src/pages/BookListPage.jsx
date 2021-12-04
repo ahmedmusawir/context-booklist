1cd8a4fc7c85e69cf7cc286efa18c19002534b61
Ahmed Musawir <moose@Ahmeds-Mac.local>
2021-12-04 20:01:04 +0800

    Block Edit done with FieldSet & useState hook - 4 Nov 2021

--- src/pages/BookListPage.jsx
+++ src/pages/BookListPage.jsx
@@ -8,10 +8,9 @@ function BookListPage() {
   const initialState = books;
   const [bookList, setBookList] = useState(initialState);
   const [isEdit, setIsEdit] = useState(true);
+  const [isSave, setIsSave] = useState(false);
 
   const handleChange = (e) => {
-    // console.log(e.target.name);
-    // console.log(e.target.value);
     const dataType = e.target.name;
 
     if (dataType === 'title') {
@@ -43,118 +42,153 @@ function BookListPage() {
         })
       );
     }
-    // console.log(bookList);
-    // console.log(e.target.id);
   };
-  //   const handleSubmit = (e) => {
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEdit((prev) => (prev = !prev));
+    setIsSave((prev) => (prev = !prev));
+  };
+  const handleSave = (e) => {
+    e.preventDefault();
+    setIsEdit((prev) => (prev = !prev));
+    setIsSave((prev) => (prev = !prev));
+  };
+
+  //   const handleSingle = (e) => {
   //     e.preventDefault();
-  //     setIsEdit((prev) => (prev = !prev));
-  //     console.log('clicked', e.target.id);
-  //     e.target.disabled = false;
+  //     const dataType = e.target.name;
+
+  //     if (dataType === 'title') {
+  //       bookList.map((book) => {
+  //         return book.id === Number(e.target.id)
+  //           ? (e.target.readOnly = false)
+  //           : null;
+  //       });
+  //     }
+  //     if (dataType === 'author') {
+  //       bookList.map((book) => {
+  //         return book.id === Number(e.target.id)
+  //           ? (e.target.readOnly = false)
+  //           : null;
+  //       });
+  //     }
+  //     if (dataType === 'description') {
+  //       bookList.map((book) => {
+  //         return book.id === Number(e.target.id)
+  //           ? (e.target.readOnly = false)
+  //           : null;
+  //       });
+  //     }
   //   };
 
-  const handleSingle = (e) => {
-    e.preventDefault();
-    // console.log(e.target);
-    const dataType = e.target.name;
-
-    if (dataType === 'title') {
-      bookList.map((book) => {
-        return book.id === Number(e.target.id)
-          ? (e.target.readOnly = false)
-          : null;
-      });
-    }
-    if (dataType === 'author') {
-      bookList.map((book) => {
-        return book.id === Number(e.target.id)
-          ? (e.target.readOnly = false)
-          : null;
-      });
-    }
-    if (dataType === 'description') {
-      bookList.map((book) => {
-        return book.id === Number(e.target.id)
-          ? (e.target.readOnly = false)
-          : null;
-      });
-    }
-  };
-
-  const handleBlur = (e) => {
-    e.preventDefault();
-    // console.log(e.target);
-    bookList.map((book) => {
-      return book.id === Number(e.target.id)
-        ? (e.target.readOnly = true)
-        : null;
-    });
-  };
+  //   const handleBlur = (e) => {
+  //     e.preventDefault();
+  //     // console.log(e.target);
+  //     bookList.map((book) => {
+  //       return book.id === Number(e.target.id)
+  //         ? (e.target.readOnly = true)
+  //         : null;
+  //     });
+  //   };
 
   return (
     <Page wide={true} pageTitle='Movie Form'>
       <Row className='justify-content-center'>
         <Col sm={12}>
           <Content width='w-100' cssClassNames='bg-light mt-3'>
+            <button
+              type='button'
+              className='btn btn-info btn-block'
+              onClick={handleEdit}
+              disabled={isSave}
+            >
+              Edit
+            </button>
+            <button
+              type='button'
+              className='btn btn-secondary btn-block mb-2'
+              onClick={handleSave}
+              disabled={isEdit}
+            >
+              Save
+            </button>
             {bookList.map((book) => (
               <main key={book.id} className='single-item'>
-                {/* <form onSubmit={handleSubmit} id={book.id}> */}
-                {/* <fieldset disabled={isEdit}> */}
-                <input
-                  id={book.id}
-                  type='text'
-                  name='title'
-                  value={book.title}
-                  className={
-                    isEdit ? 'form-control normal-view' : 'form-control'
-                  }
-                  onChange={handleChange}
-                  onClick={handleSingle}
-                  onBlur={handleBlur}
-                  readOnly={true}
-                />
-                <div
-                  className={
-                    isEdit
-                      ? 'alert alert-primary d-none'
-                      : 'alert alert-primary d-none'
-                  }
-                >
-                  {book.title}
-                </div>
-
-                <input
-                  id={book.id}
-                  type='text'
-                  name='author'
-                  value={book.author}
-                  className='form-control normal-view'
-                  onChange={handleChange}
-                  onClick={handleSingle}
-                  onBlur={handleBlur}
-                  readOnly={true}
-                />
-                <div className='alert alert-primary d-none'>{book.author}</div>
-                <textarea
-                  id={book.id}
-                  type='text'
-                  name='description'
-                  value={book.description}
-                  className='form-control normal-view'
-                  rows={3}
-                  onChange={handleChange}
-                  onClick={handleSingle}
-                  onBlur={handleBlur}
-                  readOnly={true}
-                />
-                <div className='alert alert-primary d-none'>
-                  {book.description}
-                </div>
-                {/* </fieldset> */}
-                {/* <button type='submit' className='btn btn-info'>
-                  Edit
-                </button> */}
-                {/* </form> */}
+                <form>
+                  <fieldset disabled={isEdit}>
+                    <label htmlFor='title'>
+                      <small className='font-weight-bold p-2 text-info'>
+                        Book Title
+                      </small>
+                    </label>
+                    <input
+                      id={book.id}
+                      type='text'
+                      name='title'
+                      value={book.title}
+                      className={
+                        isEdit ? 'form-control normal-view' : 'form-control'
+                      }
+                      onChange={handleChange}
+                      //   onClick={handleSingle}
+                      //   onBlur={handleBlur}
+                      //   readOnly={true}
+                    />
+                    <div
+                      className={isEdit ? 'd-none' : 'alert alert-secondary'}
+                    >
+                      {book.title}
+                    </div>
+                    <label htmlFor='author'>
+                      <small className='font-weight-bold p-2 text-info'>
+                        Author:
+                      </small>
+                    </label>
+                    <input
+                      id={book.id}
+                      type='text'
+                      name='author'
+                      value={book.author}
+                      className={
+                        isEdit ? 'form-control normal-view' : 'form-control'
+                      }
+                      onChange={handleChange}
+                      //   onClick={handleSingle}
+                      //   onBlur={handleBlur}
+                      //   readOnly={true}
+                    />
+                    <div
+                      className={isEdit ? 'd-none' : 'alert alert-secondary'}
+                    >
+                      {book.author}
+                    </div>
+                    <label htmlFor='description'>
+                      <small className='font-weight-bold p-2 text-info'>
+                        Description
+                      </small>
+                    </label>
+                    <textarea
+                      id={book.id}
+                      type='text'
+                      name='description'
+                      value={book.description}
+                      className={
+                        isEdit ? 'form-control normal-view' : 'form-control'
+                      }
+                      rows={3}
+                      onChange={handleChange}
+                      //   onClick={handleSingle}
+                      //   onBlur={handleBlur}
+                      //   readOnly={true}
+                    />
+                    <div
+                      className={isEdit ? 'd-none' : 'alert alert-secondary'}
+                    >
+                      {book.description}
+                    </div>
+                  </fieldset>
+                </form>
               </main>
             ))}
           </Content>
